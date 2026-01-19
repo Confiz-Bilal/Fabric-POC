eed28aefa5daac3a01eca03797b24c5982b54b8b
Confiz-Bilal <muhammad.bilal06@almajdouie.com>
2026-01-19 18:47:23 +0500

    Committing 2 items from workspace 0dec1b85-1750-4636-9b5a-7a51186126c7

--- notebooks/Notebook_2.Notebook/notebook-content.py
+++ notebooks/Notebook_2.Notebook/notebook-content.py
@@ -8,12 +8,12 @@
 # META   },
 # META   "dependencies": {
 # META     "lakehouse": {
-# META       "default_lakehouse": "3fe1fe6c-bc24-494d-bb6e-a377e75e8706",
+# META       "default_lakehouse": "5140e695-a8ff-40ac-a183-c2a643970467",
 # META       "default_lakehouse_name": "poc_lakehouse",
 # META       "default_lakehouse_workspace_id": "0dec1b85-1750-4636-9b5a-7a51186126c7",
 # META       "known_lakehouses": [
 # META         {
-# META           "id": "3fe1fe6c-bc24-494d-bb6e-a377e75e8706"
+# META           "id": "5140e695-a8ff-40ac-a183-c2a643970467"
 # META         }
 # META       ]
 # META     }
@@ -35,8 +35,22 @@
 
 # CELL ********************
 
-df = spark.sql("SELECT * FROM poc_lakehouse.dbo.fact_call_center_agent_evaluation LIMIT 1000")
-display(df)
+# MAGIC %%sql
+# MAGIC CREATE TABLE Fact_Call_Center_Agent_Evaluation
+# MAGIC (
+# MAGIC     ROW_FID                                   INT,
+# MAGIC     Agent_ID                                  string
+# MAGIC )
+
+# METADATA ********************
+
+# META {
+# META   "language": "sparksql",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
 
 # METADATA ********************
 
