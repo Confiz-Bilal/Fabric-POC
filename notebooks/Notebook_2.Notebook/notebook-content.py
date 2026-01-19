22a4bb52ef22d2374574bb368375df7dbe8ca033
Confiz-Bilal <muhammad.bilal06@almajdouie.com>
2026-01-19 18:24:38 +0500

    Committing 2 items from workspace a6750b8f-b31c-4d2b-ae94-444aedccd4ae

--- notebooks/Notebook_2.Notebook/notebook-content.py
+++ notebooks/Notebook_2.Notebook/notebook-content.py
@@ -6,7 +6,18 @@
 # META   "kernel_info": {
 # META     "name": "synapse_pyspark"
 # META   },
-# META   "dependencies": {}
+# META   "dependencies": {
+# META     "lakehouse": {
+# META       "default_lakehouse": "3fe1fe6c-bc24-494d-bb6e-a377e75e8706",
+# META       "default_lakehouse_name": "poc_lakehouse",
+# META       "default_lakehouse_workspace_id": "0dec1b85-1750-4636-9b5a-7a51186126c7",
+# META       "known_lakehouses": [
+# META         {
+# META           "id": "3fe1fe6c-bc24-494d-bb6e-a377e75e8706"
+# META         }
+# META       ]
+# META     }
+# META   }
 # META }
 
 # CELL ********************
@@ -14,6 +25,18 @@
 # Welcome to your new notebook
 # Type here in the cell editor to add code!
 
+
+# METADATA ********************
+
+# META {
+# META   "language": "python",
+# META   "language_group": "synapse_pyspark"
+# META }
+
+# CELL ********************
+
+df = spark.sql("SELECT * FROM poc_lakehouse.dbo.fact_call_center_agent_evaluation LIMIT 1000")
+display(df)
 
 # METADATA ********************
 
